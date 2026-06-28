908cc42eacdd59b4760f2a4174089d9f587d67df
mehdichamani <mahdi.chamani20@gmail.com>
2026-06-28 12:03:56 +0330

    env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,7 @@ module.exports = {
       script: './backend/server.js',
       instances: 'max',
       exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3001,
-        DATABASE_URL: 'postgresql://postgres:postgrespassword@localhost:5432/edari'
-      }
+      env_file: '.env'
     }
   ]
 };
