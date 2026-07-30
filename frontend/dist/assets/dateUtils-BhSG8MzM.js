64519b3432b182ff698b83caa7657a2f4150d277
mehdichamanni <mahdi.chamani20@gmail.com>
2026-07-30 10:23:25 +0330

    افزودن پوشه خروجی کامپایل فرانت‌اند (frontend/dist) به gitignore و حذف آن از tracking گیت
    - قرار دادن مسیر frontend/dist/ در فایل .gitignore
    - خارج کردن خروجی‌های ساخت فرانت‌اند از ردیابی گیت جهت جلوگیری از شلوغی repository و تداخل‌های کامیت

--- frontend/dist/assets/dateUtils-BhSG8MzM.js
+++
@@ -1,1 +0,0 @@
-import{m as t}from"./index-Bq76YnX_.js";function a(r,m="jYYYY/jMM/jDD"){return r?t(r).format(m):"-"}function n(r){return r?t(r).format("jYYYY/jMM/jDD - HH:mm"):"-"}export{n as a,a as t};
