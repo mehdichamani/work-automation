5d9f892fc50031f2ae2b6f40d6bc82d27476721f
afshin <afshinnajjari@gmail.com>
2026-08-16 13:06:46 +0330

    بالا آمدن

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,13 @@ module.exports = {
       script: './backend/server.js',
       instances: 1,
       exec_mode: 'fork',
-      env_file: path.join(__dirname, '.env'),
+      cwd: path.join(__dirname, 'backend'),
+      env: {
+        NODE_ENV: 'production',
+        PORT: 2833,
+        BIND_HOST: '0.0.0.0',
+        CORS_ORIGIN: 'http://localhost:2833,http://192.168.1.9:2833,http://172.20.2.212:2833,http://172.30.39.198:2833'
+      },
       max_memory_restart: '500M'
     }
   ]
